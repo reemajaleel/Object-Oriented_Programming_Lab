7428395303bb619ab8f06ff12af864283437a7aa
REEMA JALEEL <93534699+reemajaleel@users.noreply.github.com>
2022-05-03 20:08:59 +0530

    Add files via upload

--- Exercise 5/BookDetails.java
+++ Exercise 5/BookDetails.java
@@ -13,150 +13,131 @@ import java.util.*;
 
 class Publisher
 {
-    String pname;
-    Publisher(String n)		//constructor
-    {
-      pname=n;
-    }
-    void display()
-   {
-      System.out.println("Publisher name : "+pname);
-
-   }
-
+	String pname;
+	Publisher(String n)		//constructor
+	{
+		pname=n;
+	}
+	void display()
+	{
+		System.out.println("Publisher name : "+pname);
+	}
 }
 
 class Book extends Publisher			//subclass of Publisher
 {
-   String title,author;
-   Book(String n,String t,String a)			//constructor
-   {
-     super(n);			//invoking parent constructor
-     title=t;
-     author=a;
-   }
-   void display()
-   {
-      super.display();			//invoking parent class display()
-      System.out.println("Book title : "+title);
-      System.out.println("Author : "+author);
-   }
-
+	String title,author;
+	Book(String n,String t,String a)			//constructor
+	{
+		super(n);			//invoking parent constructor
+		title=t;
+		author=a;
+	}
+	void display()
+	{
+		super.display();			//invoking parent class display()
+		System.out.println("Book title : "+title);
+		System.out.println("Author : "+author);
+	}
 }
-
 
 class Literature extends Book			//subclass of book
 {
-    String type;
-    Literature(String n,String t,String a,String typ)				//constructor
-    {
-       super(n,t,a);			//invoking parent constructor
-       type=typ;
-    }
-     void display()
-   {
-      super.display();				//invoking parent class display
-      System.out.println("Type : "+type);
-
-   }
+	String type;
+	Literature(String n,String t,String a,String typ)				//constructor
+	{
+		super(n,t,a);			//invoking parent constructor
+		type=typ;
+	}
+	void display()
+	{
+		super.display();				//invoking parent class display
+		System.out.println("Type : "+type);
+	}
 }
-
-
 
 class Fiction extends Book			//subclass of book
 {
-     String genre;
-      Fiction(String n,String t,String a,String r)				//constructor
-      {
-         super(n,t,a);			//invoking parent constructor
-         genre=r;
-       }
-      void display()
-      {
-       super.display();				//invoking parent class display
-      System.out.println("Genre : "+genre);
-
-     }
+	String genre;
+	Fiction(String n,String t,String a,String r)				//constructor
+	{
+		super(n,t,a);			//invoking parent constructor
+		genre=r;
+	}
+	void display()
+	{
+		super.display();				//invoking parent class display
+		System.out.println("Genre : "+genre);
+	}
 }
 
 class BookDetails
 {
-    public static void main(String args[])
-    {
-       Scanner s=new Scanner(System.in);
-      System.out.println("Enter Number of Books of literature: ");
-     
-      int n1=s.nextInt();
-      System.out.println("Enter Number of Books of Fiction: ");
-     
-      int n2=s.nextInt();
-     
-   
-     
-      Book ref;
-         Literature obj1[]=new Literature[n1];
-         Fiction obj2[]=new Fiction[n2];
-         int i=0;
-         int j=0;
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter Number of Books of literature: ");
+		int n1=s.nextInt();
+		System.out.println("Enter Number of Books of Fiction: ");
+		int n2=s.nextInt();
+		Book ref;
+			Literature obj1[]=new Literature[n1];
+			Fiction obj2[]=new Fiction[n2];
+			int i=0;
+			int j=0;
  
-        
-         do{
-            System.out.println("\n\nEnter Literature Book "+(i+1)+" details");
-           System.out.println("Enter Publisher name :  ");
-           String p=s.next();
-           System.out.println("Enter Book name :  ");
-           String bk=s.next();
-           System.out.println("Enter Author name :  ");
-           String au=s.next();
-           
-           System.out.println("Enter Literature type :  ");
-           String lt=s.next();
-           obj1[i]=new Literature(p,bk,au,lt);
-         i++;
-          }while(i<n1);
+			do{
+				System.out.println("\n\nEnter Literature Book "+(i+1)+" details");
+				System.out.println("Enter Publisher name :  ");
+				String p=s.next();
+				System.out.println("Enter Book name :  ");
+				String bk=s.next();
+				System.out.println("Enter Author name :  ");
+				String au=s.next();
+				System.out.println("Enter Literature type :  ");
+				String lt=s.next();
+				obj1[i]=new Literature(p,bk,au,lt);
+				i++;
+			}while(i<n1);
          
-       
-     
-           do  {
-                System.out.println("\n\nEnter Fiction Book "+(j+1)+" details");
-           System.out.println("Enter Publisher name :  ");
-           String p=s.next();
-           System.out.println("Enter Book name :  ");
-           String bk=s.next();
-           System.out.println("Enter Author name :  ");
-           String au=s.next();
-           
-           System.out.println("Enter Fiction type :  ");
-           String lt=s.next();
-           obj2[j]=new Fiction(p,bk,au,lt);
-         j++;
-
-         }while(j<n2);
-       
-     while(true){
-     System.out.println("\n\n1.LITERATURE\n2.FICTION\n3.EXIT\n\nEnter work type to show....\n");
-             int ch2=s.nextInt();
-    switch(ch2){
-        case 1:
-           for(i=0;i<n1;i++)
-        {
-               System.out.println("\n____LITERATURE BOOK "+ (i+1)+" INFORMATION ___\n");
-               obj1[i].display();
-             }
-       break;
-        case 2:
-                  for(j=0;j<n2;j++){
-               System.out.println("\n____FICTION BOOK "+(j+1)+" INFORMATION ___\n");
-               obj2[j].display();    
-         }
-        break;
-    case 3:System.exit(0);
-      default:System.out.println("invalid");
-       
-      }
-
-     }
-}
+			do{
+				System.out.println("\n\nEnter Fiction Book "+(j+1)+" details");
+				System.out.println("Enter Publisher name :  ");
+				String p=s.next();
+				System.out.println("Enter Book name :  ");
+				String bk=s.next();
+				System.out.println("Enter Author name :  ");
+				String au=s.next();
+				System.out.println("Enter Fiction type :  ");
+				String lt=s.next();
+				obj2[j]=new Fiction(p,bk,au,lt);
+				j++;
+			}while(j<n2);
+			while(true){
+				System.out.println("\n\n1.LITERATURE\n2.FICTION\n3.EXIT\n\nEnter work type to show....\n");
+				int ch2=s.nextInt();
+				switch(ch2){
+					case 1:
+						for(i=0;i<n1;i++)
+						{
+							System.out.println("\n____LITERATURE BOOK "+ (i+1)+" INFORMATION ___\n");
+							obj1[i].display();
+						}
+						break;
+					case 2:
+						for(j=0;j<n2;j++)
+						{
+							System.out.println("\n____FICTION BOOK "+(j+1)+" INFORMATION ___\n");
+							obj2[j].display();    
+						}
+						break;
+					default:
+						System.out.println("Invalid");
+						System.out.println("Exiting.....");
+						System.exit(0);
+					}
+				}
+		}
 }
              
 /*
@@ -247,4 +228,6 @@ Genre : Novel
 Enter work type to show....
 
 3
+Invalid
+Exiting.....
 */
